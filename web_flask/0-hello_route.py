2d2ccc1d7d060de175737796511d81b13931e27c
homemix <kenmutati@gmail.com>
2022-04-08 16:10:42 +0300

    0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+""" Flask Hello Route """
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello():
+def HBNB():
+    """ Hello, HBNB! """
     return " Hello HBNB!"
 
 
