83785d24de6b11ee847cf99db7173cadee5c4ced
homemix <kenmutati@gmail.com>
2022-04-08 16:18:18 +0300

    a script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Flask Hello Route """
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def HBNB():
+    """ Hello, HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNB_2():
+    """ Hello, HBNB! """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def C_text(text):
+    """ C text """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
